27f1dc2a0b8217e072d857de5268b778ed963903
Jordan Matthiesen <jomatthi@microsoft.com>
2016-10-27 21:08:14 -0700

    Pulling data from Azure App Services; added support for mocking in ionic serve.

--- demos/investmentTracker/src/providers/account-data.ts
+++ demos/investmentTracker/src/providers/account-data.ts
@@ -22,7 +22,10 @@ export class AccountData {
   }
 
   login() {
-    this.getAzureClient().login("aad").done(this.loginResponse.bind(this));
+    // Only enforce login when on an actual device
+    if (WindowsAzure) {
+      this.getAzureClient().login("aad").done(this.loginResponse.bind(this));
+    }
   }
 
   private loginResponse(response: Microsoft.WindowsAzure.User) {
@@ -33,42 +36,25 @@ export class AccountData {
   }
 
   load(): any {
-
-    //if (typeof(WindowsAzure) == "undefined") {
+    if (typeof (WindowsAzure) == "undefined") {
       return new Promise(resolve => {
         // We're using Angular Http provider to request the data,
         // then on the response it'll map the JSON data to a parsed JS object.
         // Next we process the data and resolve the promise with the new data.
         this.http.get('assets/data/data.json').subscribe(res => {
-          // we've got back the raw data, now generate the core schedule data
-          // and save the data for later reference
-          this.data = this.processData(res.json());
+          this.data = res.json();
           resolve(this.data);
         });
       });
-  //  } else {
-  //    if (!this.azureAppService) {
-  //      this.azureAppService = new WindowsAzure.MobileServiceClient("https://tacoinvestmenttracker.azurewebsites.net");
-  //    }
-  //  }
-    
-  //  let accounts = this.getAzureClient().getTable("Accounts");
-  // return accounts.read().then((data) => {
-  //    debugger;
-  //  });
-
-    //// don't have the data yet
-    //return new Promise(resolve => {
-    //  // We're using Angular Http provider to request the data,
-    //  // then on the response it'll map the JSON data to a parsed JS object.
-    //  // Next we process the data and resolve the promise with the new data.
-    //  this.http.get('assets/data/data.json').subscribe(res => {
-    //    // we've got back the raw data, now generate the core schedule data
-    //    // and save the data for later reference
-    //    this.data = this.processData(res.json());
-    //    resolve(this.data);
-    //  });
-    //});
+    } else {
+      let accounts = this.getAzureClient().getTable("Accounts");
+      return new Promise(resolve => {
+        accounts.read().then((data) => {
+          this.data = this.processData(data);
+          resolve(this.data);
+        });
+      });
+    }
   }
 
   addAccount(account: any) {
@@ -79,22 +65,19 @@ export class AccountData {
     return this.azureAppService.getTable("Accounts").insert(newAccount);
   }
 
-  processData(data) {
-    // just some good 'ol JS fun with objects and arrays
-    // build up the data by linking speakers to sessions
+  processData(data: Array<any>) {
+    let output = {accounts: [], investments: []};
+    data.forEach(account => {
+      output.accounts.push(
+        {
+          id: account.id,
+          name: account.name,
+          type: account.type
+        }
+      )
+    });
 
-    // loop through each day in the schedule
-//    data.schedule.forEach(day => {
-//      // loop through each timeline group in the day
-//      day.groups.forEach(group => {
-//        // loop through each session in the timeline group
-//        group.sessions.forEach(session => {
-//          this.processSession(data, session);
-//        });
-//      });
-//    });
-
-    return data;
+    return output;
   }
 
   processSession(data, session) {
@@ -124,72 +107,46 @@ export class AccountData {
 
   getAccounts() {
     return this.load().then(data => {
-      return data.accounts; 
+      return data.accounts;
     });
-  //  return this.load().then(data => {
-  //    let day = data.schedule[dayIndex];
-  //    day.shownSessions = 0;
-
-  //    queryText = queryText.toLowerCase().replace(/,|\.|-/g, ' ');
-  //    let queryWords = queryText.split(' ').filter(w => !!w.trim().length);
-
-  //    day.groups.forEach(group => {
-  //      group.hide = true;
-
-  //      group.sessions.forEach(session => {
-  //        // check if this session should show or not
-  //        session.favorite = this.user.hasFavorite(session.name);
-  //        this.filterSession(session, queryWords, excludeTracks, segment);
-
-  //        if (!session.hide) {
-  //          // if this session is not hidden then this group should show
-  //          group.hide = false;
-  //          day.shownSessions++;
-  //        }
-  //      });
-
-  //    });
-
-  //    return day;
-  //  });
   }
 
   filterSession(session, queryWords, excludeTracks, segment) {
 
-  //  let matchesQueryText = false;
-  //  if (queryWords.length) {
-  //    // of any query word is in the session name than it passes the query test
-  //    queryWords.forEach(queryWord => {
-  //      if (session.name.toLowerCase().indexOf(queryWord) > -1) {
-  //        matchesQueryText = true;
-  //      }
-  //    });
-  //  } else {
-  //    // if there are no query words then this session passes the query test
-  //    matchesQueryText = true;
-  //  }
+    //  let matchesQueryText = false;
+    //  if (queryWords.length) {
+    //    // of any query word is in the session name than it passes the query test
+    //    queryWords.forEach(queryWord => {
+    //      if (session.name.toLowerCase().indexOf(queryWord) > -1) {
+    //        matchesQueryText = true;
+    //      }
+    //    });
+    //  } else {
+    //    // if there are no query words then this session passes the query test
+    //    matchesQueryText = true;
+    //  }
 
-  //  // if any of the sessions tracks are not in the
-  //  // exclude tracks then this session passes the track test
-  //  let matchesTracks = false;
-  //  session.tracks.forEach(trackName => {
-  //    if (excludeTracks.indexOf(trackName) === -1) {
-  //      matchesTracks = true;
-  //    }
-  //  });
+    //  // if any of the sessions tracks are not in the
+    //  // exclude tracks then this session passes the track test
+    //  let matchesTracks = false;
+    //  session.tracks.forEach(trackName => {
+    //    if (excludeTracks.indexOf(trackName) === -1) {
+    //      matchesTracks = true;
+    //    }
+    //  });
 
-  //  // if the segement is 'favorites', but session is not a user favorite
-  //  // then this session does not pass the segment test
-  //  let matchesSegment = false;
-  //  if (segment === 'favorites') {
-  //    if (this.user.hasFavorite(session.name)) {
-  //      matchesSegment = true;
-  //    }
-  //  } else {
-  //    matchesSegment = true;
-  //  }
+    //  // if the segement is 'favorites', but session is not a user favorite
+    //  // then this session does not pass the segment test
+    //  let matchesSegment = false;
+    //  if (segment === 'favorites') {
+    //    if (this.user.hasFavorite(session.name)) {
+    //      matchesSegment = true;
+    //    }
+    //  } else {
+    //    matchesSegment = true;
+    //  }
 
-  //  // all tests must be true if it should not be hidden
-  //  session.hide = !(matchesQueryText && matchesTracks && matchesSegment);
+    //  // all tests must be true if it should not be hidden
+    //  session.hide = !(matchesQueryText && matchesTracks && matchesSegment);
   }
 }
